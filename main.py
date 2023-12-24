079718d2902e5afbf6f7c7fbc97949c21c73df99
danchik332 <144592714+danchik332@users.noreply.github.com>
2023-12-24 12:48:48 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+messages = input("введите сообщение") #создаем переменную в которую пишем строку
+print(messages.title()) #выводим (я использовал команды из интернета)
+print(messages.upper())
+print(messages.lower())
